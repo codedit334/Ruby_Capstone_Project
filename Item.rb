d61959a804372ed70f26f5ca902163a45a4fb239
codedit334 <oussama7elabdioui@gmail.com>
2023-05-29 16:07:26 +0100

    Make can_be_archived? a private method

--- Item.rb
+++ Item.rb
@@ -12,25 +12,14 @@ class Item
     @archived = archived
   end
 
+  def move_to_archive()
+    @archived = true if(can_be_archived?)
+  end
+
+  private
+
   def can_be_archived?()
     return false unless publish_date > 10
     true
   end
-
-  def move_to_archive()
-    @archived = true if(can_be_archived?)
-  end
 end
-
-# can_be_archived?() in the Item class
-# should return true if published_date is older than 10 years
-# otherwise, it should return false
-# move_to_archive() in the Item class
-# should reuse can_be_archived?() method
-# should change the archived property to true if the result of the can_be_archived?() method is true
-# should do nothing if the result of the
-# can_be_archived?() method is false
-
-# Create a main.rb file that will serve as your console app entry-point.
-# Implement startup actions:
-# Present the user with a list of options to perform.
