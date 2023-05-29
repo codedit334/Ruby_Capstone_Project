c1111a3acf4e70f23629293ad51484fe0c76d2ad
codedit334 <oussama7elabdioui@gmail.com>
2023-05-29 16:06:07 +0100

    Create can_be_archived? method

--- Item.rb
+++ Item.rb
@@ -1,17 +1,34 @@
 class Item 
-initialize (genre, author, source, label, publish_date, archived)
-  @genre = genre
-  genre.items << self
-  @author = author
-  author.items << self
-  @source = source
-  source.items << self
-  @label = label
-  label.items << self
-  @publish_date = publish_date
-  publish_date.items << self
-  @archived = archived
-  archived.items << self
+  initialize(genre, author, source, label, publish_date, archived) do
+    @genre = genre
+    genre.items << self
+    @author = author
+    author.items << self
+    @source = source
+    source.items << self
+    @label = label
+    label.items << self
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def can_be_archived?()
+    return false unless publish_date > 10
+    true
+  end
+
+ 
 end
 
-end
+# can_be_archived?() in the Item class
+# should return true if published_date is older than 10 years
+# otherwise, it should return false
+# move_to_archive() in the Item class
+# should reuse can_be_archived?() method
+# should change the archived property to true if the result of the can_be_archived?() method is true
+# should do nothing if the result of the
+# can_be_archived?() method is false
+
+# Create a main.rb file that will serve as your console app entry-point.
+# Implement startup actions:
+# Present the user with a list of options to perform.
