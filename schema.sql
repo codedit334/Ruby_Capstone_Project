60b4d77a9f7b63c7e3ea63ca580e5aa7c3f6179c
codedit334 <oussama7elabdioui@gmail.com>
2023-05-31 14:39:56 +0100

    Create book table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE label(
  PRIMARY KEY(id)
 );
 
+-- Create book table  
+CREATE TABLE book(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publisher VARCHAR(50),
+  cover_state VARCHAR(15),
+  publish_date DATE,
+  archived BOOLEAN,
+  label_id  INT,
+  CONSTRAINT fk_book FOREIGN KEY (label_id) REFERENCES label (id) ON DELETE CASCADE,
+  PRIMARY KEY(id)
+);
