e7bcd61b2c6830a631a4399c01413009ddb21ec4
SaneMyburg <sane.myburg@gmail.com>
2023-05-31 15:38:35 +0200

    Updated item class

--- Item.rb
+++ Item.rb
@@ -1,24 +1,25 @@
+require 'date'
+
 class Item
   attr_accessor :genre, :authour, :source, :label, :publish_date, :archived, :id
 
-  initialize(genre, author, source, label, publish_date, archived) do
-    @id = rand(1 .. 100)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @publish_date = publish_date
+  def initialize(publish_date, archived: false)
+    @id = rand(1..100)
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = Date.parse(publish_date)
     @archived = archived
   end
 
   def move_to_archive()
-    @archived = true if(can_be_archived?)
+    @archived = true if can_be_archived?
   end
 
   private
 
   def can_be_archived?()
-    return false unless publish_date > 10
-    true
+    Time.now.year - @publish_date.year > 10
   end
 end
