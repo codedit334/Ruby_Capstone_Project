17d1a73727802931c4faeaaba4b0d0acd799b4ed
SaneMyburg <sane.myburg@gmail.com>
2023-06-01 10:57:06 +0200

    Update music class

--- app.rb
+++ app.rb
@@ -18,10 +18,11 @@ class App
 
   def list_music_albums
     puts 'Music Albums:'
+    puts @music
     @music.each do |album|
-      puts "Title: #{album[:title]}"
-      puts "On Spotify: #{album[:on_spotify] ? 'Yes' : 'No'}"
-      puts "Publication Date: #{album[:publish_date]}"
+      puts "On Spotify: #{album.on_spotify ? 'Yes' : 'No'}"
+      puts "Publication Date: #{album.publish_date}"
+      puts "Title: #{album.name}"
     end
   end
 
@@ -37,12 +38,12 @@ class App
     on_spotify = gets.chomp.downcase == 'y'
 
     puts 'Enter the title of the album:'
-    title = gets.chomp
+    name = gets.chomp
 
     puts 'Enter the publication date for the album (yyyy/mm/dd):'
     publish_date = gets.chomp
 
-    album = { title: title, on_spotify: on_spotify, publish_date: publish_date }
+    album = MusicAlbum.new(on_spotify, publish_date, name)
     @music << album
     save_data
 
@@ -50,7 +51,7 @@ class App
   end
 
   def save_data
-    File.write('music.json', JSON.generate(@music))
+    File.write('music.json', JSON.generate(@music.map(&:to_h)))
 
     File.write('genre.json', JSON.generate(@genre.map(&:to_hash)))
   end
@@ -58,7 +59,14 @@ class App
   def load_data
     if File.exist?('music.json')
       music_data = JSON.parse(File.read('music.json'))
-      @music = music_data.is_a?(Array) ? music_data : []
+      @music = if music_data.is_a?(Array)
+                 music_data.map do |data|
+                   MusicAlbum.new(data['on_spotify'], data['publish_date'],
+                                  data['name'])
+                 end
+               else
+                 []
+               end
     end
 
     return unless File.exist?('genre.json')
@@ -69,9 +77,9 @@ class App
 end
 
 app = App.new
+# app.add_music_album
 app.list_music_albums
-app.list_genres
-app.add_music_album
-app.create_genre
-app.list_music_albums
-app.list_genres
+# app.list_genres
+# app.create_genre
+# app.list_music_albums
+# app.list_genres
--- music_classes/music.rb
+++ music_classes/music.rb
@@ -2,10 +2,12 @@ require_relative '../item'
 
 class MusicAlbum < Item
   attr_reader :on_spotify
+  attr_accessor :name
 
-  def initialize(on_spotify, publish_date)
+  def initialize(on_spotify, publish_date, name)
     super(publish_date)
     @on_spotify = on_spotify
+    @name = name
   end
 
   def can_be_archived?
@@ -14,5 +16,13 @@ class MusicAlbum < Item
 
   def move_to_archive
     @archived = true if can_be_archived?
+  end
+
+  def to_h
+    {
+      on_spotify: @on_spotify,
+      publish_date: @publish_date,
+      name: @name
+    }
   end
 end
