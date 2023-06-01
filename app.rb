7e6be5beb3f406c8a43070c873473322f9345a53
atok624 <84607674+atok624@users.noreply.github.com>
2023-06-01 10:18:10 +0000

    Fixed linter errors

--- app.rb
+++ app.rb
@@ -1,23 +1,59 @@
-require './Book/book'
-require './Book/label'
-require_relative 'game/game_ui'
+require_relative './music_classes/music'
+require_relative './music_classes/genre'
+require 'json'
 
 class App
   def initialize
-    @book = []
-    @label = [Label.new('New', 'yellow'), Label.new('Gift', 'green')]
-    @game = GameUI.new
+    @music = []
+    @genre = []
+    load_data
   end
 
-  def list_all_books
-    @book.each do |book|
-      label = @label.find { |label| label.items.include?(book) }
+  def create_genre
+    puts 'Enter genre:'
+    genre = gets.chomp
+    @genre << Genre.new(genre)
+    save_data
+  end
 
-      puts "Publisher: #{book.publisher}"
-      puts "Cover State: #{book.cover_state}"
-      puts "Publish Date: #{book.publish_date}"
-      puts "Label: #{label.title}" if label
+  def list_music_albums
+    puts 'Music Albums:'
+    puts @music
+    @music.each do |album|
+      puts "On Spotify: #{album.on_spotify ? 'Yes' : 'No'}"
+      puts "Publication Date: #{album.publish_date}"
+      puts "Title: #{album.name}"
     end
+  end
+
+  def list_genres
+    puts 'Genres:'
+    @genre.each do |genre|
+      puts genre.name
+    end
+  end
+
+  def add_music_album
+    puts 'Is the music album available on Spotify? [y/n]'
+    on_spotify = gets.chomp.downcase == 'y'
+
+    puts 'Enter the title of the album:'
+    name = gets.chomp
+
+    puts 'Enter the publication date for the album (yyyy/mm/dd):'
+    publish_date = gets.chomp
+
+    album = MusicAlbum.new(on_spotify, publish_date, name)
+    @music << album
+    save_data
+
+    puts 'Music album created successfully.'
+  end
+
+  def save_data
+    File.write('music.json', JSON.generate(@music.map(&:to_h)))
+
+    File.write('genre.json', JSON.generate(@genre.map(&:to_hash)))
   end
 
   def list_games
@@ -28,47 +64,32 @@ class App
     @game.list_authors
   end
 
-  def list_all_labels
-    @label.each_with_index do |label, index|
-      puts "#{index + 1}) Title: #{label.title}, Color: #{label.color}"
-    end
-  end
-
-  def create_book
-    puts 'Enter book publisher:'
-    publisher = gets.chomp
-
-    p 'Enter book cover state:'
-    cover_state = gets.chomp
-
-    p 'Enter book publish date (YYYY-MM-DD):'
-    publish_date = gets.chomp
-
-    book = Book.new(publisher, cover_state, publish_date)
-    @book.push(book)
-
-    p 'Book created successfully'
-
-    p "Do you want to add a label to #{publisher}'s book? (y/n)"
-    answer = gets.chomp
-    if %w[y Y].include?(answer)
-      list_all_labels
-
-      p 'Choose a label index:'
-
-      label_index = gets.chomp.to_i - 1
-      @label[label_index].add_item(book)
-
-      p 'Book added to label successfully'
+  def load_data
+    if File.exist?('music.json')
+      music_data = JSON.parse(File.read('music.json'))
+      @music = if music_data.is_a?(Array)
+                 music_data.map do |data|
+                   MusicAlbum.new(data['on_spotify'], data['publish_date'],
+                                  data['name'])
+                 end
+               else
+                 []
+               end
     end
 
-    p 'Thanks for using our app'
+    return unless File.exist?('genre.json')
+
+    genre_data = JSON.parse(File.read('genre.json'))
+    @genre = genre_data.is_a?(Array) ? genre_data.map { |data| Genre.new(data['name']) } : []
   end
 end
 
 app = App.new
-app.list_all_labels
-app.create_book
-app.list_all_books
-app.list_authors
+# app.add_music_album
+app.list_music_albums
 app.list_games
+app.list_authors
+# app.list_genres
+# app.create_genre
+# app.list_music_albums
+# app.list_genres
